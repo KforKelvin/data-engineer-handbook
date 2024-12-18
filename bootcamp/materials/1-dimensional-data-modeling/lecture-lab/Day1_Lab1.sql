0494696406c8101809197421612092c6415f930e
KforKelvin <ky1015@tamu.edu>
2024-12-18 17:47:28 -0600

    update

--- bootcamp/materials/1-dimensional-data-modeling/lecture-lab/Day1_Lab1.sql
+++ bootcamp/materials/1-dimensional-data-modeling/lecture-lab/Day1_Lab1.sql
@@ -6,7 +6,7 @@ CREATE TYPE season_stats AS (
 			 pts REAL,
 			 reb REAL,
 			 ast REAL
-                         
+
 );
 
 -- drop type season_stats cascade ;
@@ -67,9 +67,9 @@ SELECT
 		)::season_stats]
 		ELSE y.season_stats
 	END as season_stats,
-	CASE 
+	CASE
 		WHEN t.season IS NOT NULL THEN
-			CASE 
+			CASE
 				WHEN t.pts > 20 THEN 'star'
 				WHEN t.pts > 15 THEN 'good'
 				WHEN t.pts > 10 THEN 'average'
@@ -77,40 +77,40 @@ SELECT
 			END::scoring_class
 		ELSE y.scoring_class
 	END AS scoring_class,
-	
+
 	CASE WHEN t.season IS NOT NULL THEN 0
-		 ELSE y.years_since_last_season + 1 
+		 ELSE y.years_since_last_season + 1
 	END AS years_since_last_season,
 
-	
+
 	COALESCE(t.season, y.current_season + 1) as current_season
 FROM today t FULL OUTER JOIN yesterday y
 ON t.player_name = y.player_name order by player_name;
 
 
 -- select * from players
--- where 
--- player_name = 'Michael Jordan' 
--- and 
--- current_season = 2000 
+-- where
+-- player_name = 'Michael Jordan'
+-- and
+-- current_season = 2000
 
 -- select player_name,
 -- 	unnest(season_stats)::season_stats as season_stats
--- 	from players 
---  where player_name = 'Michael Jordan' and current_season = 2001 
+-- 	from players
+--  where player_name = 'Michael Jordan' and current_season = 2001
 
 -- WITH UNNESTED AS (
 -- 	select player_name,
 -- 	unnest(season_stats)::season_stats as season_stats
--- 	from players 
+-- 	from players
 -- 	where  player_name = 'Michael Jordan'
 -- 	and  current_season = 2001
-	
+
 -- )
 
 select player_name,
 	(season_stats[CARDINALITY(season_stats)]::season_stats).pts /
-	CASE WHEN (season_stats[1]::season_stats).pts = 0 THEN 1 ELSE (season_stats[1]::season_stats).pts END 
+	CASE WHEN (season_stats[1]::season_stats).pts = 0 THEN 1 ELSE (season_stats[1]::season_stats).pts END
 from players
 where current_season = 2001 and scoring_class = 'star'
 -- order by 2 DESC
